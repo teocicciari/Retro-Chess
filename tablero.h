73459435c74ec5210d8e3b7bef17dd2ce242f1a5
wubadubalublub <30708578+wubadubalublub@users.noreply.github.com>
2018-07-03 02:04:02 -0300

    mover pieza

--- tablero.h
+++ tablero.h
@@ -7,6 +7,10 @@ tablero_t tablero_vacio();
 
 tablero_t inicializar_tablero(tablero_t tablero);
 
+tablero_t destroy_tablero(tablero_t tablero);
+
+tablero_t hacer_mov(tablero_t tablero, char piezaname, char colOrig, int filaOrig, char colDest, int filaDest);
+
 void mostrar_tablero(tablero_t tablero);
 
 piezas_t pos_inicial(piezas_t piezas);
