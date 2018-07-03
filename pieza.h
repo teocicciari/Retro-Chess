db2bdecd896eb74816f2c07954025f5a11dd7599
wubadubalublub <30708578+wubadubalublub@users.noreply.github.com>
2018-07-03 02:02:36 -0300

    mover pieza

--- pieza.h
+++ pieza.h
@@ -7,6 +7,10 @@ piezas_t piezas_empty();
 
 piezas_t nueva_pieza(piezas_t piezas, int f, char c, char n);
 
+piezas_t destroy_piezas(piezas_t pieza);
+
+piezas_t mover_pieza(piezas_t piezas, char piezaname, char colOrig, int filaOrig, char colD, int fD);
+
 char pieza_column(piezas_t pieza);
 
 int pieza_fila(piezas_t pieza);
